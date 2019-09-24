d23f28fe3a0b6fc2f7084732162758335282e08c
anjana6 <anjanashakthi95@gmail.com>
2019-09-24 07:47:25 +0530

    create slide button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-    $('.preloader').fadeOut();
+    $('.preloader').fadeOut()
 
     $('.navBtn').click(function(){
           $('.nav').toggleClass('nav-show');
+    })
+
+    $('.video-switch-container').click(function(){
+        const value =  $('.video-switch-btn').hasClass('btnSlide')
+        console.log(value);
+        if(value){
+            $('.video-switch-btn').removeClass('btnSlide')
+            $('#video').get(0).play()
+
+        }else{
+            $('.video-switch-btn').addClass('btnSlide')
+            $('#video').get(0).pause()
+        }
     })
 
 }) 
