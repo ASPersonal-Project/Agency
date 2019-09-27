d5a57886d7485c258f80da3459c5e11aec494154
anjana6 <anjanashakthi95@gmail.com>
2019-09-27 20:40:23 +0530

    create js part of teme section

--- js/app.js
+++ js/app.js
@@ -33,6 +33,64 @@ $(document).ready(function(){
 
         $(this).toggleClass('rotate')
         $(this).parent().next().slideToggle()
-      })
+      });
+
+      (function(){
+          let data = [
+              {
+                  id:0,
+                  name:'john doe',
+                  job:'developer',
+                  text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, laudantium quia! Porro minus quo accusantium laboriosam eveniet repellat ipsa facere autem ipsum eos modi, nesciunt quaerat ut excepturi alias nobis.',
+                  favorites:['eat','drink','sleep'],
+                  img:'images/per1.jpg'
+              },
+              {
+                id:1,
+                name:'silva sho',
+                job:'writter',
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, laudantium quia! Porro minus quo accusantium laboriosam eveniet repellat ipsa facere autem ipsum eos modi, nesciunt quaerat ut excepturi alias nobis.',
+                favorites:['run','swim','jump'],
+                img:'images/per2.jpg'
+            },
+            {
+                id:2,
+                name:'rubi wosn',
+                job:'reporter',
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, laudantium quia! Porro minus quo accusantium laboriosam eveniet repellat ipsa facere autem ipsum eos modi, nesciunt quaerat ut excepturi alias nobis.',
+                favorites:['drink','run','sleep'],
+                img:'images/per3.jpg'
+            },
+            {
+                id:3,
+                name:'chanu rib',
+                job:'engineer',
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, laudantium quia! Porro minus quo accusantium laboriosam eveniet repellat ipsa facere autem ipsum eos modi, nesciunt quaerat ut excepturi alias nobis.',
+                favorites:['walk','drink','sleep'],
+                img:'images/per4.jpg'
+            }
+          ]
+        $('.team-img-item').click(function(){
+            $('.team-img-item').not(this).removeClass('active')
+            $(this).addClass('active')
+            // console.log("hai");
+            let id = $(this).attr('data-id')
+            console.log(id);
+            $('.team-info-name').text(data[id].name)
+            $('.team-info-job').text(data[id].job)
+            $('.team-info-text').text(data[id].text)
+            $('.team-info-single-favo-eat').each(function(index){
+                $(this).text(data[id].favorites[index])
+            
+            })
+            $('.team-info-img').attr('src',data[id].img)
+        })
+
+
+
+
+
+      })();
+
 
 })  
